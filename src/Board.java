376b908badcc90680db80db3bd006f4a0e50f0a8
Sinclair <borshon.sadafshahid@gmail.com>
2020-10-20 00:33:17 +0600

    Tried making a boardGUI

---
+++ src/Board.java
@@ -0,0 +1,12 @@
+public class Board {
+    public int[][] positions;
+    boolean checkGameState(int[][] positions){
+        return false;
+    }
+    boolean updateGameState(int[][] positions) {
+        return false;
+    }
+    boolean isKingChecked(){
+        return false;
+    }
+}
