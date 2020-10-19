98272eebddad1374c86cdd481715d6d532f7f07a
Rafsan <rafsan.quayes@gmail.com>
2020-10-20 00:45:16 +0600

    Did some stuff

--- src/TestMain.java
+++ src/TestMain.java
@@ -1,5 +1,16 @@
+import javax.swing.*;
+
 public class TestMain {
     public static void main(String[] args) {
-        System.out.println("just checking");
+        JFrame jf = new JFrame("Game");
+        jf.setSize(1200,1200);
+        jf.setResizable(false);
+        jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        boardGUI b = new boardGUI();
+        jf.add(b.BackPanel);
+
+        jf.setVisible(true);
+
     }
 }
--- src/boardGUI.java
+++ src/boardGUI.java
@@ -1,7 +1,24 @@
+import Pieces.Pawn;
+import Pieces.Piece;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class boardGUI {
-    private JPanel BackPanel;
+    public JPanel BackPanel;
     private JButton button1;
 
+    private Pawn pawn1;
+
+    public boardGUI() {
+        pawn1 = new Pawn();
+        button1.setIcon(pawn1.getWhiteImage());
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }
