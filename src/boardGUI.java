d4ffc64179d80ff6fd5ac4e834eb240a7729e253
Rafsan <rafsan.quayes@gmail.com>
2020-10-20 00:57:24 +0600

    Pawn Icons are alright

--- src/boardGUI.java
+++ src/boardGUI.java
@@ -8,6 +8,9 @@ import java.awt.event.ActionListener;
 public class boardGUI {
     public JPanel BackPanel;
     private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
 
     private Pawn pawn1;
 
@@ -15,6 +18,15 @@ public class boardGUI {
         pawn1 = new Pawn();
         button1.setIcon(pawn1.getWhiteImage());
         button1.setFocusPainted(false);
+        button2.setIcon(pawn1.getWhiteImage());
+        button2.setFocusPainted(false);
+
+        button3.setIcon(pawn1.getBlackImage());
+        button3.setFocusPainted(false);
+
+        button4.setIcon(pawn1.getBlackImage());
+        button4.setFocusPainted(false);
+
         button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
