a05e7bb1625d2207984e653fbeb3748a0449c711
Afzal Siddique <afzal.siddique99@gmailcom>
2020-10-30 22:29:07 +0600

    spot class implements comparable. This might be useful for testing

--- src/Pieces/Spot.java
+++ src/Pieces/Spot.java
@@ -1,9 +1,33 @@
 package Pieces;
 
-public class Spot {
+import java.util.Comparator;
+
+public class Spot implements Comparable<Spot> {
     public int row, col;
     public Spot(int row, int col){
         this.row = row;
         this.col = col;
     }
+
+//    use collections.sort(spotList) for testing availableMoves
+    @Override
+    public int compareTo(Spot spot) {
+        if(this.row>spot.row)
+            return 1;
+        if(this.row<spot.row)
+            return -1;
+        return 0;
+    }
+
+
+
+//    delete this
+//    @Override
+//    public int compare(Spot spot1, Spot spot2) {
+//        if(spot1.row<spot2.row)
+//            return 1;
+//        else if(spot1.row>spot2.row)
+//            return -1;
+//        return 0;
+//    }
 }
