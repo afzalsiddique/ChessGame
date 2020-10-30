c4c8057af8d9807cad8d99df3478b381c0c9a280
Afzal Siddique <afzal.siddique99@gmailcom>
2020-10-30 21:21:48 +0600

    ..

--- src/Pieces/Pawn.java
+++ src/Pieces/Pawn.java
@@ -16,12 +16,14 @@ public class Pawn extends Piece {
     public ArrayList<Spot> calculateAllPossibleMoves() {
 
         availableMoves.clear();
-        if (spot.row >=0 && spot.row <=7 && spot.col >=0 && spot.col <=7)
-            availableMoves.add(new Spot(spot.row -1, spot.col));
-        if (spot.row >=0 && spot.row <=7 && spot.col >=0 && spot.col <=7)
-            availableMoves.add(new Spot(spot.row -1, spot.col -1));
-        if (spot.row >=0 && spot.row <=7 && spot.col >=0 && spot.col <=7)
-            availableMoves.add(new Spot(spot.row -1, spot.col +1));
+        if(isWhite) {
+            if (spot.row >= 0 && spot.row <= 7 && spot.col >= 0 && spot.col <= 7)
+                availableMoves.add(new Spot(spot.row - 1, spot.col));
+            if (spot.row >= 0 && spot.row <= 7 && spot.col >= 0 && spot.col <= 7)
+                availableMoves.add(new Spot(spot.row - 1, spot.col - 1));
+            if (spot.row >= 0 && spot.row <= 7 && spot.col >= 0 && spot.col <= 7)
+                availableMoves.add(new Spot(spot.row - 1, spot.col + 1));
+        }
         return  availableMoves;
     }
 
