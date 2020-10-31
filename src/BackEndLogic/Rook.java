3f90dc1f8cfcbc764650da80843d040ba016aeca
Afzal Siddique <afzal.siddique99@gmailcom>
2020-10-31 16:33:33 +0600

    test for rook move not working

--- src/BackEndLogic/Rook.java
+++ src/BackEndLogic/Rook.java
@@ -23,39 +23,13 @@ public class Rook extends Piece
         {
             for (int row = spot.row + 1; row < 8; row++)
             {
-                if (board.positions[row][spot.col] != null) //Enemy piece not implemented
+                if (board.positions[row][spot.col] == null) //Enemy piece not implemented
                     availableMoves.add(new Spot(row, spot.col));
-                else if (!board.positions[row][spot.col].isWhite)
-                    availableMoves.add(new Spot(row, spot.col));
-                else
+                else {
+                    if (!(board.positions[row][spot.col].isWhite))
+                        availableMoves.add(new Spot(row, spot.col));
                     break;
-            }
-            for (int row = spot.row - 1; row >= 0; row--)
-            {
-                if (board.positions[row][spot.col] != null)
-                    availableMoves.add(new Spot(row, spot.col));
-                else if (!board.positions[row][spot.col].isWhite)
-                    availableMoves.add(new Spot(row, spot.col));
-                else
-                    break;
-            }
-            for (int col = spot.col + 1; col < 8; col++)
-            {
-                if (board.positions[spot.row][col] != null)
-                    availableMoves.add(new Spot(spot.row, col));
-                else if (!board.positions[spot.row][col].isWhite)
-                    availableMoves.add(new Spot(spot.row, col));
-                else
-                    break;
-            }
-            for (int col = spot.col - 1; col >= 0; col--)
-            {
-                if (board.positions[spot.row][col] != null)
-                    availableMoves.add(new Spot(spot.row, col));
-                else if (!board.positions[spot.row][col].isWhite)
-                    availableMoves.add(new Spot(spot.row, col));
-                else
-                    break;
+                }
             }
         }
         return  availableMoves;
