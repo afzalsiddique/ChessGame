9cd87b8de62110f12cf8cb5d6adfbc4d65ab9cfc
Sinclair <borshon.sadafshahid@gmail.com>
2020-10-31 16:06:36 +0600

    Rook moves

--- src/BackEndLogic/Rook.java
+++ src/BackEndLogic/Rook.java
@@ -2,34 +2,68 @@ package BackEndLogic;
 
 import java.util.ArrayList;
 
-public class Rook extends Piece {
-    public Rook(boolean isWhite, Spot spot){
+public class Rook extends Piece
+{
+    public Rook(boolean isWhite, Spot spot)
+    {
         this.isWhite = isWhite;
         this.spot = spot;
     }
     @Override
-    public boolean makeMove(Piece[][] positions) {
+    public boolean makeMove(Piece[][] positions)
+    {
         return super.makeMove(positions);
     }
 
     @Override
-    public ArrayList<Spot> calculateAllPossibleMoves(Board board) {
+    public ArrayList<Spot> calculateAllPossibleMoves(Board board)
+    {
         availableMoves.clear();
-        for(int row = 0;row<8;row++){
-            if(spot.row == row)
-                continue;
-            availableMoves.add(new Spot(row,spot.col));
-        }
-        for(int col = 0;col<8;col++){
-            if(spot.row == col)
-                continue;
-            availableMoves.add(new Spot(spot.row,col));
+        if (isWhite)
+        {
+            for (int row = spot.row + 1; row < 8; row++)
+            {
+                if (board.positions[row][spot.col] != null) //Enemy piece not implemented
+                    availableMoves.add(new Spot(row, spot.col));
+                else if (!board.positions[row][spot.col].isWhite)
+                    availableMoves.add(new Spot(row, spot.col));
+                else
+                    break;
+            }
+            for (int row = spot.row - 1; row >= 0; row--)
+            {
+                if (board.positions[row][spot.col] != null)
+                    availableMoves.add(new Spot(row, spot.col));
+                else if (!board.positions[row][spot.col].isWhite)
+                    availableMoves.add(new Spot(row, spot.col));
+                else
+                    break;
+            }
+            for (int col = spot.col + 1; col < 8; col++)
+            {
+                if (board.positions[spot.row][col] != null)
+                    availableMoves.add(new Spot(spot.row, col));
+                else if (!board.positions[spot.row][col].isWhite)
+                    availableMoves.add(new Spot(spot.row, col));
+                else
+                    break;
+            }
+            for (int col = spot.col - 1; col >= 0; col--)
+            {
+                if (board.positions[spot.row][col] != null)
+                    availableMoves.add(new Spot(spot.row, col));
+                else if (!board.positions[spot.row][col].isWhite)
+                    availableMoves.add(new Spot(spot.row, col));
+                else
+                    break;
+            }
         }
         return  availableMoves;
     }
 
 
-    public void castling(){
+    public void castling()
+    {
 
     }
 }
