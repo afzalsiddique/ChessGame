b76be6951a8d32969f723ac4fbf43cc3a4a312a9
Afzal Siddique <afzal.siddique99@gmailcom>
2020-10-31 15:37:34 +0600

    ...

--- src/TestSpotSort.java
+++ src/TestSpotSort.java
@@ -1,29 +1,26 @@
 import BackEndLogic.Spot;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class TestSpotSort {
-    public static void main(String[] args) {
-        ArrayList<Spot> spotList1 = new ArrayList<>(){
-            {
-                add(new Spot(5,1));
-                add(new Spot(5,0));
-            }
-        };
-        ArrayList<Spot> spotList2 = new ArrayList<>(){
-            {
-                add(new Spot(5,0));
-                add(new Spot(5,1));
-            }
-        };
-        Collections.sort(spotList1);
-        Collections.sort(spotList2);
-        for(Spot spot:spotList1)
-            System.out.println(spot.row+" "+spot.col);
-        System.out.println("\n");
-        for(Spot spot:spotList2)
-            System.out.println(spot.row+" "+spot.col);
-
-    }
+class TestSpotSort{
+        @Test
+        public void test1(){
+            ArrayList<Spot> actual = new ArrayList<>(){
+                {
+                    add(new Spot(5,1));
+                    add(new Spot(5,0));
+                }
+            };
+            Collections.sort(actual);
+            ArrayList<Spot> expected = new ArrayList<>(){
+                {
+                    add(new Spot(5,0));
+                    add(new Spot(5,1));
+                }
+            };
+            Assert.assertArrayEquals(expected.toArray(), actual.toArray());
+        }
 }
