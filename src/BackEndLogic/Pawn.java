700c73d2107bc1ed40ace2de985eb9836f73b074
Sinclair <borshon.sadafshahid@gmail.com>
2020-11-22 20:31:17 +0600

    I don't know what i did, but i did something

--- src/BackEndLogic/Pawn.java
+++ src/BackEndLogic/Pawn.java
@@ -18,9 +18,9 @@ public class Pawn extends Piece {
         if(isWhite) {
             if (row - 1 >= 0 && !board.isOccupied(new Spot(row-1, col)))
                 availableMoves.add(new Spot(row - 1, col));
-            if (row - 1 >= 0 && col - 1 >= 0 && board.isOccupied(new Spot(row-1, col-1)) && board.isOpponent(this,positions[row-1][col-1]))
+            if (row - 1 >= 0 && col - 1 >= 0  && board.isOpponent(this,positions[row-1][col-1]))
                 availableMoves.add(new Spot(row - 1, col - 1));
-            if (row - 1 >= 0 && col + 1 <= 7 && board.isOccupied(new Spot(row-1, col+1)) && board.isOpponent(this,positions[row-1][col+1]))
+            if (row - 1 >= 0 && col + 1 <= 7  && board.isOpponent(this,positions[row-1][col+1]))
                 availableMoves.add(new Spot(row - 1, col + 1));
         }
         return  availableMoves;
