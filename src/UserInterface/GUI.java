dbd159c96ebdcae1d346cf67acb22c2f803fe57d
Rafsan <rafsan.quayes@gmail.com>
2020-11-25 12:18:28 +0600

    Made methods in GUI

--- src/UserInterface/GUI.java
+++ src/UserInterface/GUI.java
@@ -6,12 +6,24 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GUI extends JFrame {
-//    public UserInterface.buttons[][] buttons = new buttons[8][8];
+    public buttons[][] buttons = new buttons[8][8];
     ActionListener actionListener;
     String move = "";
+
+    boolean isSelectedState = false;
+
     public GUI(){
         createButtons();
         makeLayoutVisible();
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    void selectPiece(int x, int y){
+
+    }
+
+    void moveSelectedPiece(int x, int y){
+
     }
 
     void createActionListener(int x, int y){
@@ -19,32 +31,42 @@ public class GUI extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 // Make moves etc.
+
+                if(isSelectedState){
+                    moveSelectedPiece(x, y);
+                }
+                else{
+                    selectPiece(x, y);
+                }
+
             }
         };
 
     }
 
     public void createButtons(){
-//        for(int i=0;i<8;i++){
-//            for(int j=0;j<8;j++){
-//                buttons[i][j] = new buttons("");
-//                add(buttons[i][j]);
-//                createActionListener(i,j);
-//                buttons[i][j].addActionListener(actionListener);
-//                if(i%2==0){
-//                    if(j%2==0)
-//                        buttons[i][j].setBackground(Color.WHITE);
-//                    else
-//                        buttons[i][j].setBackground(Color.BLACK);
-//                }
-//                else {
-//                    if (j%2!=0)
-//                        buttons[i][j].setBackground(Color.WHITE);
-//                    else
-//                        buttons[i][j].setBackground(Color.BLACK);
-//                }
-//            }
-//        }
+        for(int i=0;i<8;i++){
+            for(int j=0;j<8;j++){
+                buttons[i][j] = new buttons("");
+                add(buttons[i][j]);
+                createActionListener(i,j);
+                buttons[i][j].addActionListener(actionListener);
+
+                // Set the color
+                if(i%2==0){
+                    if(j%2==0)
+                        buttons[i][j].setBackground(Color.WHITE);
+                    else
+                        buttons[i][j].setBackground(Color.BLACK);
+                }
+                else {
+                    if (j%2!=0)
+                        buttons[i][j].setBackground(Color.WHITE);
+                    else
+                        buttons[i][j].setBackground(Color.BLACK);
+                }
+            }
+        }
     }
 
     private void makeLayoutVisible() {
