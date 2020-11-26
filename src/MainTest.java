63d1e78c492d3a116974cea84c92de99a896f1a5
Sinclair <borshon.sadafshahid@gmail.com>
2020-11-26 20:08:05 +0600

    Bishop Done

--- src/MainTest.java
+++ src/MainTest.java
@@ -9,11 +9,15 @@ public class MainTest {
        Piece pawn2 = new Pawn(false, 1,1);
        Piece rook1 = new Rook(true, new Spot(7,0));
        Piece rook2 = new Rook(false, new Spot(4,4));
+       Piece bishop1 = new Bishop(true, new Spot(0,0));
+       Piece bishop2 = new Bishop(false, new Spot(0,7));
 
        board.addPiece(pawn1);
        board.addPiece(pawn2);
        board.addPiece(rook1);
        board.addPiece(rook2);
+       board.addPiece(bishop1);
+       board.addPiece(bishop2);
 
        board.addPiece(new King(true, new Spot(3,5)));
 
