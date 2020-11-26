fa030691766a551401b3a4d77faf144266a34ab2
Afzal Siddique <afzal.siddique99@gmailcom>
2020-11-26 14:12:59 +0600

    deadcode removed

--- src/TestAI_Agent.java
+++ src/TestAI_Agent.java
@@ -2,30 +2,7 @@ import BackEndLogic.*;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Collections;
-
 public class TestAI_Agent {
-    @Test
-    public void test1(){
-        Board board = new Board();
-        Piece piece1 = new Pawn(true, new Spot(6,1));
-        Piece piece2 = new Pawn(true, new Spot(6,2));
-        board.addPiece(piece1);
-        board.addPiece(piece2);
-        AI_Agent ai_agent = new AI_Agent(board);
-        ArrayList<Spot> actual = ai_agent.getAllAvailableMovesOfEveryPiece();
-        Collections.sort(actual);
-
-        ArrayList<Spot> expected = new ArrayList<>(){
-            {
-                add(new Spot(5,1));
-                add(new Spot(5,2));
-            }
-        };
-        Assert.assertArrayEquals(expected.toArray(), actual.toArray());
-    }
-
     @Test
     public void test2(){
         Board board = new Board();
