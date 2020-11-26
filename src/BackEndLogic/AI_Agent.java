f831719fa2ddca32cca057e6aa076c22b29f4fb8
Afzal Siddique <afzal.siddique99@gmailcom>
2020-11-26 11:18:00 +0600

    minimax algorithm implemented although not sure if it works

--- src/BackEndLogic/AI_Agent.java
+++ src/BackEndLogic/AI_Agent.java
@@ -2,12 +2,68 @@ package BackEndLogic;
 
 import java.util.ArrayList;
 
+import static java.lang.Integer.max;
+
 public class AI_Agent {
     Board board;
 
-    public AI_Agent(Board currentBoard) {
-        this.board = currentBoard;
+    public AI_Agent(Board currentGameBoard) {
+        this.board = currentGameBoard;
     }
+    public int minimax(Board board, int depth, boolean maxPlayer){ // first call to this method should be with !maxPlayer
+        Spot spotPieceSource;
+//        spotPieceDest is after availableMoves method is called
+        Piece pieceSource;
+        Piece pieceDest; // could be null
+        int score = evaluate(board);
+        if(depth==0)
+            return score;
+        // if someone wins
+        if(score>10000 || score < -10000) // assuming winning score is greater than
+            return score;
+
+        // if someone hasn't won or max depth hasn't reached
+        Piece[][] positions = board.positions;
+        if(maxPlayer){
+            int maxScore = -999999;
+            for(int i=0;i<8;i++){
+                for(int j=0;j<8;j++){
+                    pieceSource = positions[i][j];
+                    if(pieceSource!=null){
+                        spotPieceSource = pieceSource.getSpot();
+                        ArrayList<Spot> availableMoves = pieceSource.calculateAllPossibleMoves();
+                        for(Spot spotPieceDest:availableMoves){
+                            pieceDest = board.getPiece(spotPieceDest);
+                            // make the move
+                            // move pieceSource to spotPieceDest
+                            movePieceToSpot(board, pieceSource, spotPieceDest);
+                            // calculate score and take the maxScore
+                            score = minimax(board, depth-1, !maxPlayer);
+                            maxScore = max(maxScore, score);
+                            // undo the move
+                            // move pieceSource to spotPieceSource
+                            movePieceToSpot(board, pieceSource, spotPieceSource);
+                            // move pieceDest to spotPieceDest
+                            movePieceToSpot(board, pieceDest, spotPieceDest);
+                        }
+                    }
+                }
+            }
+            return maxScore;
+        }
+        else{
+            int minScore = 999999;
+
+            return minScore;
+        }
+    }
+    public void movePieceToSpot(Board board, Piece piece, Spot spot){
+        int row = spot.row;
+        int col = spot.col;
+        board.positions[row][col] = piece;
+    }
+
+    // remove this method
     public ArrayList<Spot> getAllAvailableMovesOfEveryPiece(){
         ArrayList<Spot> allMoves = new ArrayList<>();
         Piece[][] positions = board.positions;
@@ -20,13 +76,16 @@ public class AI_Agent {
         }
         return allMoves;
     }
-    public int evaluate(Board board){ // this can evaluate any board not just current board
+    public int evaluate(Board board){ // this can evaluate any board not just current game board
+        // need to calculate checkmate score
+        // if white wins score should be greater than 10000 or must be changed in minimax method
+        // if white loses score should be less than -10000 or must be changed in minimax method
         Piece[][] positions = board.positions;
-        int evaluation = 0;
+        int score = 0;
         for(int i=0;i<8;i++)
             for(int j=0;j<8;j++)
                 if(positions[i][j] != null)
-                    evaluation += positions[i][j].getValue();
-        return evaluation;
+                    score += positions[i][j].getValue();
+        return score;
     }
 }
