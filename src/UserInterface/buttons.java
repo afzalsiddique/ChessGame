a3f7dc7c52ccdc8b34ad1b60a13f2767c6dbfc2a
Rafsan <rafsan.quayes@gmail.com>
2020-12-01 22:04:36 +0600

    Refactored buttons

--- src/UserInterface/buttons.java
+++ src/UserInterface/buttons.java
@@ -2,18 +2,11 @@ package UserInterface;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class buttons extends JButton implements ActionListener {
+public class buttons extends JButton {
     Dimension dim = new Dimension(80, 80);
     buttons(){
        setPreferredSize(dim);
-       addActionListener(this);
        setEnabled(true);
-    }
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
     }
 }
