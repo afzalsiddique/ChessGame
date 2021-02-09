4dc80d13b93e03b1b49d824d556b700b11ce07d4
Rafsan <rafsan.quayes@gmail.com>
2021-02-09 12:46:08 +0600

    En Passant works

--- src/BackEndLogic/Game.java
+++ src/BackEndLogic/Game.java
@@ -40,6 +40,13 @@ public class Game {
         backEndBoard.addPiece(new King(true, new Spot(7,4)));
         backEndBoard.addPiece(new King(false, new Spot(0,4)));
         backEndBoard.addPiece(new Bishop(true, new Spot(7,2)));
+        backEndBoard.addPiece(new Bishop(true, new Spot(7,5)));
+        backEndBoard.addPiece(new Bishop(false, new Spot(0,2)));
+        backEndBoard.addPiece(new Bishop(false, new Spot(0,5)));
+        backEndBoard.addPiece(new Queen(true, new Spot(7,3)));
+        backEndBoard.addPiece(new Queen(false, new Spot(0,3)));
+        backEndBoard.addPiece(new Knight(true, new Spot(7,1)));
+        backEndBoard.addPiece(new Knight(true, new Spot(7,6)));
 
         for(int i=1; i<8; i++){
             backEndBoard.addPiece(new Pawn(false, new Spot(1,i)));
