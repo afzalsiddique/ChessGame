4664b338b07fbbb9844460bf761994c1113ed855
Rafsan <rafsan.quayes@gmail.com>
2021-02-09 12:47:31 +0600

    Full board setup

--- src/BackEndLogic/Game.java
+++ src/BackEndLogic/Game.java
@@ -47,12 +47,14 @@ public class Game {
         backEndBoard.addPiece(new Queen(false, new Spot(0,3)));
         backEndBoard.addPiece(new Knight(true, new Spot(7,1)));
         backEndBoard.addPiece(new Knight(true, new Spot(7,6)));
+        backEndBoard.addPiece(new Knight(false, new Spot(0,6)));
+        backEndBoard.addPiece(new Knight(false, new Spot(0,1)));
 
-        for(int i=1; i<8; i++){
+        for(int i=0; i<8; i++){
             backEndBoard.addPiece(new Pawn(false, new Spot(1,i)));
         }
 
-        for(int i=1; i<8; i++){
+        for(int i=0; i<8; i++){
             backEndBoard.addPiece(new Pawn(true, new Spot(6,i)));
         }
 
