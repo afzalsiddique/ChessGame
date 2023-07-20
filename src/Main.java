9d5045752bd28e8d903636baf681c979723c9835
Rafsan <rafsan.quayes@gmail.com>
2023-07-21 00:41:37 +0600

    Renamed the file to Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import UserInterface.*;
 import java.util.Random;
 
 // Just creates a window with the menu
-public class Demo_1_MainClass {
+public class Main {
     public static void main(String[] args) {
         DisplayWindow displayWindow = new DisplayWindow();
         MenuGui menuGui = new MenuGui();
